9c37589954fe695fe46aae40f82b961611e15e2b
lppduy <lephamphuongduy@gmail.com>
2023-09-05 11:14:56 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import TaskForm from './components/TaskForm';
+
+const App = () => {
+  return (
+    <div className="app">
+      <TaskForm />
+      <main className="app_main">
+        <section className="task_column">Section 1</section>
+        <section className="task_column">Section 2</section>
+        <section className="task_column">Section 3</section>
+      </main>
+    </div>
+  );
+};
+
+export default App;
